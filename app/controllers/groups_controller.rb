275dc818452daef1f4efc2b5400d22bd381dd549
kkk293590509 <k293590509@gmail.com>
2017-03-17 04:48:44 +0800

    implement groups#destroy

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -15,9 +15,11 @@ end
  end
   def create
    @group = Group.new(group_params)
-   @group.save
-
-     redirect_to groups_path
+  if  @group.save
+redirect_to groups_path
+else
+  render :new
+ end
  end
    def update
      @group = Group.find(params[:id])
@@ -34,9 +36,7 @@ end
     redirect_to groups_path
   end
  private
-
  def group_params
    params.require(:group).permit(:title, :description)
  end
-
 end
